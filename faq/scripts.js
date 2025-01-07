2e6666793eaecc3d28417804a4cd3def072411ba
Shnior Amir <shniramir@gmail.com>
2025-01-07 16:13:12 +0200

    add file info

--- faq/scripts.js
+++ faq/scripts.js
@@ -1,17 +1,4 @@
-/* function toggleMenu() {
-    const menu = document.getElementById('accessibility-menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
 
-function changeFontSize(factor) {
-    document.body.style.fontSize =
-        (parseFloat(window.getComputedStyle(document.body).fontSize) + factor) + 'px';
-}
-
-function toggleHighContrast() {
-    document.body.classList.toggle('high-contrast');
-}
- */
 const questionsContainer = document.getElementById("questions-container");
 const userQuestionInput = document.getElementById("user-question");
 const submitQuestionBtn = document.getElementById("submit-question");
